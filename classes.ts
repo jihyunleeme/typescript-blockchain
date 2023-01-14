f04dde5a62c89fec2db32c858272866af4f5c878
jihyunlee <jihyunlee.me@gmail.com>
2023-01-15 00:18:26 +0900

    #4.1 classes recap

--- classes.ts
+++ classes.ts
@@ -31,3 +31,37 @@ console.log(nico.getFullName());
 // private 클래스 밖에서 불가능, 상속한 자식 클래서에서 접근 불가능
 // protected 클래스 밖에서 불가능하지만, 자식 클래스에서 접근 가능!
 
+/* # 4.1 */
+// 추상 메서드 구현이 되어있지 않은 (코드가 없는) 메소드 - 상속 받는 클래스에서 추상 메소드 구현
+
+type Words = {
+  [key: string]: string;
+};
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+
+  add(word: Word) {
+    // class를 타입처럼 사용
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] === word.def;
+    }
+  }
+
+  def(term: string) {
+    return this.words[term];
+  }
+}
+
+class Word {
+  constructor(public term: string, public def: string) {}
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+
+const dict = new Dict();
+dict.add(kimchi);
+dict.def("kimchi");
+
